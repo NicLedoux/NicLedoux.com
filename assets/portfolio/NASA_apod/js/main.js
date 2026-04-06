b438568cb6ca6576b28d8208b51c5de9fac02ce9
NicLedoux <NL15025100@gmail.com>
2026-04-06 09:52:39 -0700

    Added js for loading direct video files

--- assets/portfolio/NASA_apod/js/main.js
+++ assets/portfolio/NASA_apod/js/main.js
@@ -23,11 +23,22 @@ function renderMedia(data) {
     document.querySelector("img").src = data.url;
     document.getElementById("iframeContainer").classList.add("hidden");
   } else if (data.media_type === "video") {
+    const isDirectVideo = /\.(mp4|webm|ogg)(\?.*)?$/i.test(data.url); // Checks file extension for video files
     document.querySelector("img").classList.add("hidden");
     document.querySelector("a").classList.add("hidden");
-    document.getElementById("iframeContainer").classList.remove("hidden");
-    document.querySelector("iframe").src = data.url;
-    document.querySelector("small").classList.add("hidden");
+
+    if (isDirectVideo) {
+      //hides iframe container if direct video file and shows video container.
+      document.getElementById("iframeContainer").classList.add("hidden");
+      document.querySelector("iframe").src = "";
+      document.getElementById("videoSource").src = data.url;
+      document.querySelector("video").load();
+      document.getElementById("videoContainer").classList.remove("hidden");
+    } else {
+      document.getElementById("videoContainer").classList.add("hidden");
+      document.getElementById("iframeContainer").classList.remove("hidden");
+      document.querySelector("iframe").src = data.url;
+    }
   } else {
     document.querySelector("img").classList.add("hidden");
     document.querySelector("a").classList.add("hidden");
